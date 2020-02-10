15378c4b416d4bc8e8e64f66f74f71bee20c5937
sthancock <steven.hancock@ed.ac.uk>
2020-02-10 18:04:03 +0000

    Pandas operation examples added.

--- week5/geopandas/readPandas.py
+++ week5/geopandas/readPandas.py
@@ -24,5 +24,14 @@ if __name__=="__main__":
   # sort by the time column
   sortedData=data.sort_values('time')
 
+  # print out the columns
+  print(sortedData.columns)
+
+  # mean of a column
+  print("mean time",sortedData['time'].mean())
+
+  # bounds
+  print("x bounds",sortedData['x'].min(),sortedData['x'].max())
 
   # possible continuation   https://gist.github.com/nygeog/2731427a74ed66ca0e420eaa7bcd0d2b
+
