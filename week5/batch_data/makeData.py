9d81788e8ecdd7268c923f3dc93fc81e2f00cb1f
sthancock <steven.hancock@ed.ac.uk>
2020-02-12 11:59:56 +0000

    Little more progress on batch data script.

--- week5/batch_data/makeData.py
+++ week5/batch_data/makeData.py
@@ -3,3 +3,28 @@
 Code to make many 
 squirrel tracks
 '''
+
+
+######################################
+
+import argparse
+
+
+######################################
+
+
+def readCommands():
+  '''
+  Read commandline arguments
+  '''
+  p = argparse.ArgumentParser(description=("Handle a set of points in geopandas"))
+  p.add_argument("--input", dest ="inName", type=str, default='../data/squirrel.csv', help=("Input filename"))
+  p.add_argument("--repeats", dest ="nRep",,type=int, default=10, help=("Number of repeat paths"))
+  cmdargs = p.parse_args()
+  return cmdargs
+
+
+######################################
+
+
+
